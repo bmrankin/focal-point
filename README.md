acfb0136be6b9f745a83834cf1568b8e1c0286df
Bryan Rankin <bryan@bryanrankin.com>
2019-03-14 21:45:57 -0400

    added ga code

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # FocalPOINT
 
+[![Netlify Status](https://api.netlify.com/api/v1/badges/e8a8c273-989d-4916-a91c-888b71c3cc86/deploy-status)](https://app.netlify.com/sites/focalpoint-calc-123/deploys)
+
 Calculator for finding the anchor height for desired optimal focal point when hanging art on a wall.
 
 ## Tech Stack
--- src/router.js
+++ src/router.js
@@ -1,16 +1,24 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import VueAnalytics from 'vue-analytics'
 import Home from './core/pages/Home'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   routes: [
     {
       path: '/',
       name: 'home',
       component: Home
-    },
+    }
   ]
 })
+
+Vue.use(VueAnalytics, {
+  id: 'UA-84679550-2',
+  router
+})
+
+export default router
